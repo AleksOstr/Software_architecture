4bd39174cb393b5cb0952272cffda4d42e861b30
AleksOstr <ostryankoam@yandex.ru>
2023-08-28 15:53:14 +0400

    Created Angle3D, Flash

--- src/main/java/ru/geekbrains/lesson1/store3D/models/Flash.java
+++ src/main/java/ru/geekbrains/lesson1/store3D/models/Flash.java
@@ -1,6 +1,25 @@
 package ru.geekbrains.lesson1.store3D.models;
 
-public class Flash {
+import java.awt.*;
 
-    //TODO: Доработать в рамках ДР
+public class Flash {
+    private Point3D location;
+    private Angle3D angle;
+    private Color color;
+    private float power;
+     public void rotate(Angle3D angle) {
+         this.angle = angle;
+     }
+
+     public void move(Point3D destination) {
+         this.location = destination;
+     }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setPower(float power) {
+        this.power = power;
+    }
 }
