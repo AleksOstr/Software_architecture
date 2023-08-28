ff96e5d384e42051c59d7f62ba4ea08a39d0a56f
AleksOstr <ostryankoam@yandex.ru>
2023-08-28 15:55:14 +0400

    Created Camera

--- src/main/java/ru/geekbrains/lesson1/store3D/models/Camera.java
+++ src/main/java/ru/geekbrains/lesson1/store3D/models/Camera.java
@@ -1,7 +1,14 @@
 package ru.geekbrains.lesson1.store3D.models;
 
 public class Camera {
+    private Point3D location;
+    private Angle3D angle;
 
-    //TODO: Доработать в рамках ДР
+    public void move(Point3D destination) {
+        this.location = destination;
+    }
 
+    public void rotate(Angle3D angle) {
+        this.angle = angle;
+    }
 }
